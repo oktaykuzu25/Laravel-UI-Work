700693a3b1079f37253c3a03023852c462ce7a4f
okti <okti@192.168.1.101>
2026-05-28 11:27:02 +0300

    crud sistemi yapıldı listeleme ekleme silme güncelleme

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -8,9 +8,35 @@
                     <div class="card-header">Books</div>
 
                     <div class="card-body">
-                        @foreach ($books as $book)
-                            <p>{{ $book->name }} - {{ $book->price }}</p>
-                        @endforeach
+                        <table class="table align-middle">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Book Name</th>
+                                    <th scope="col">Book Price</th>
+                                    <th scope="col" class="text-end">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($books as $book)
+                                    <tr>
+                                        <th scope="row">{{ $book->id }}</th>
+                                        <td>{{ $book->name }}</td>
+                                        <td>{{ $book->price }}</td>
+                                        <td class="text-end">
+                                            <div class="d-flex justify-content-end gap-2">
+                                                <a href="{{ route('books.edit', $book->id) }}" class="btn btn-sm btn-primary">Edit</a>
+                                                <form action="{{ route('books.destroy', $book->id) }}" method="POST" onsubmit="return confirm('Bu kitabı silmek istediğinize emin misiniz?');">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-sm btn-danger">Delete</button>
+                                                </form>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@ Route::prefix('admin')->middleware('auth')->group(function () {
     Route::get('/books', [BookController::class, 'index'])->name('books.index');
     Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
     Route::post('/books/store', [BookController::class, 'store'])->name('books.store');
+    Route::get('/books/{id}', [BookController::class, 'edit'])->name('books.edit');
+    Route::put('/books/{id}', [BookController::class, 'update'])->name('books.update');
+    Route::delete('/books/{id}', [BookController::class, 'destroy'])->name('books.destroy');
 });
 Auth::routes();
 
